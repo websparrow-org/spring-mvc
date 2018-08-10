90292db4e1159555c68b4254b7ddd27d1782799a
Atul Rai <iatulrai@gmail.com>
2018-08-10 20:34:16 +0530

    Spring 5 MVC Hello World Example using XML configuration

---
+++ spring5-mvc-hello/src/main/java/org/websparrow/HelloController.java
@@ -0,0 +1,24 @@
+package org.websparrow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String name = request.getParameter("name");
+
+		Map<String, String> map = new HashMap<>();
+		map.put("name", name);
+
+		return new ModelAndView("welcome", map);
+	}
+
+}
---
+++ spring5-mvc-hello/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+	<h2>Spring 5 MVC Hello World Example using XML configuration</h2>
+
+	<form action="hello.htm" method="post">
+
+		Name: <input name="name" type="text" />
+		      <input value="Submit" type="submit" />
+
+	</form>
+	
+</body>
+</html>
