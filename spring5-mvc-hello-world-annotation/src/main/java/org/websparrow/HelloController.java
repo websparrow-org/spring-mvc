7da1dd8408cd1e68e02fbdee6c61db98bd561a8b
Atul Rai <Atul Rai@iatulrai@gmail.com>
2018-08-11 12:08:05 +0530

    Spring 5 MVC Hello World Example using Annoatation

---
+++ spring5-mvc-hello-world-annotation/src/main/java/org/websparrow/HelloController.java
@@ -0,0 +1,25 @@
+package org.websparrow;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello.htm")
+	public ModelAndView greet(@RequestParam("name") String name) throws Exception {
+
+		ModelAndView mv = new ModelAndView();
+		
+		// name of JSP page
+		mv.setViewName("welcome");
+
+		// data that you want to show on JSP
+		mv.addObject("name", name);
+
+		return mv;
+	}
+
+}
