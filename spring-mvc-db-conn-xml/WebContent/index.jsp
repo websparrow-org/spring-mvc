4af0cf448de0f82657fec217be7a61f1d50bec86
Atul Rai <iatulrai@gmail.com>
2018-09-05 22:05:21 +0530

    Spring mvc database connection using xml configuration

---
+++ spring-mvc-db-conn-xml/WebContent/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+	<h2>Spring MVC Database Connectivity Example using XML configuration</h2>
+	<b>Message:</b> ${msg}
+</body>
+</html>
--- spring-mvc-db-conn-xml/src/main/java/org/websparrow/controller/DbController.java
+++ spring-mvc-db-conn-xml/src/main/java/org/websparrow/controller/DbController.java
@@ -5,32 +5,32 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-import org.websparrow.dao.DbAccess;
 
 public class DbController implements Controller {
 
+	DataSource dataSource;
+
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-		
-
 		Map<String, String> map = new HashMap<String, String>();
-		
-		DbAccess db = new DbAccess();
 
-		if (db.connect() != null) {
+		if (dataSource.getConnection() != null) {
 			map.put("msg", "Database connection established successfully.");
 
 		} else {
 			map.put("msg", "Failed to connect database.");
 
 		}
-		
-		
+
 		return new ModelAndView("index", map);
 
 	}
-
 }
